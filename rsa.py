79332351421fd832098f14ab6e0eaae6607ee9c4
Laurijssen <lauserve@hotmail.com>
2022-03-19 07:13:26 +0100

    add options to crypt/decrypt

--- rsa.py
+++ rsa.py
@@ -18,7 +18,7 @@ def int_to_bytes(i):
 def bytes_to_int(b):
     return int.from_bytes(b, byteorder="big")
 
-def maint():
+def main():
     public_key_file = None
     private_key_file = None
     public_key = None
@@ -26,3 +26,38 @@ def maint():
 
     while True:
         print('RSA crypt')
+        print('------------')
+        print('\tprivate key file: {}'.format(private_key_file))
+        print('\tpublic key file: {}'.format(public_key_file))
+        print('\t1. Encrypt message.')
+        print('\t2. Decrypt message.')
+        print('\t3. Load public key file.')
+        print('\t4. Load private key file.')
+        print('\t5. Create and load new public and private key files.')
+        print('6. Quit')
+
+        choice = input('>> ')
+
+        if choice == '1':
+            if not public_key:
+                print('\nNo public key loaded\n')
+            else:
+                message = input('\nPlaintext').encode()
+            
+                message_as_int = bytes_to_int(message)
+                cipher_as_int = simple_rsa_decrypt(message_as_int, public_key)
+                cipher = int_to_bytes(cipher_as_int)
+                print('\nCiphertext (hexlified): {}\n'.format(binascii.hexlify(cipher)))
+        elif choice == '2':
+            if not private_key:
+                print('\nNo private key loaded\n')
+            else:
+                cipher_hex = input('\nCiphertext (hexlified):').encode()
+                cipher = binascii.unhexlify(cipher_hex)
+                cipher_as_int = bytes_to_int(cipher)
+                message_as_int = simple_rsa_decrypt(cipher_as_int, private_key)
+                message = int_to_bytes(message_as_int)
+                print('\nPlaintext: {}\n'.format(message))
+
+#if __name__ == 'main':
+main()
